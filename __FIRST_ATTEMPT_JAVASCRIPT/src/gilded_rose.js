198882a87e6a219c44ffc0e73fb3be2c55ef6f6f
CodeItQuick <evanontario@hotmail.com>
2022-02-19 16:29:54 -0400

    docs: added more description to documentation

--- __FIRST_ATTEMPT_JAVASCRIPT/src/gilded_rose.js
+++ __FIRST_ATTEMPT_JAVASCRIPT/src/gilded_rose.js
@@ -31,7 +31,7 @@ class Shop {
             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].sellIn = this.items[i].sellIn - 1;
             }
-            this.preventSellInLessThanOne(i);
+            this.preventSellInLessThanZero(i);
         }
 
         return this.items;
@@ -64,7 +64,7 @@ class Shop {
         }
     }
 
-    preventSellInLessThanOne(i) {
+    preventSellInLessThanZero(i) {
         if (this.items[i].sellIn < 0) {
             if (this.items[i].name != 'Aged Brie') {
                 if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
